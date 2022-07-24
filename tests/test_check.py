f533cc7cd01ac326c1a696757ae7d085d6ea92a1
Patrick Arminio <patrick.arminio@gmail.com>
2022-07-24 15:42:57 +0100

    Cleaner tests

--- tests/test_check.py
+++ tests/test_check.py
@@ -5,63 +5,62 @@ from autopub import Autopub
 from autopub.exceptions import ReleaseFileEmpty, ReleaseFileNotFound, ReleaseNoteInvalid, MissingReleaseType, InvalidReleaseType
 
 
-def test_check_fails_if_no_release_file_is_present(tmpdir):
+@pytest.fixture
+def temporary_working_directory(tmpdir):
     with tmpdir.as_cwd():
-        autopub = Autopub()
-
-        with pytest.raises(ReleaseFileNotFound):
-            autopub.check()
+        yield tmpdir
 
 
-def test_works(tmpdir, valid_release_text: str):
-    release_file = Path(tmpdir) / "RELEASE.md"
-    release_file.write_text(valid_release_text)
+def test_check_fails_if_no_release_file_is_present(temporary_working_directory):
+    autopub = Autopub()
 
-    with tmpdir.as_cwd():
-        autopub = Autopub()
-
+    with pytest.raises(ReleaseFileNotFound):
         autopub.check()
 
 
-def test_fails_if_release_note_is_empty(tmpdir):
-    release_file = Path(tmpdir) / "RELEASE.md"
+def test_works(temporary_working_directory, valid_release_text: str):
+    release_file = Path(temporary_working_directory) / "RELEASE.md"
+    release_file.write_text(valid_release_text)
+
+    autopub = Autopub()
+    autopub.check()
+
+
+def test_fails_if_release_note_is_empty(temporary_working_directory):
+    release_file = Path(temporary_working_directory) / "RELEASE.md"
     release_file.write_text("")
 
-    with tmpdir.as_cwd():
-        autopub = Autopub()
+    autopub = Autopub()
 
-        with pytest.raises(ReleaseFileEmpty):
-            autopub.check()
+    with pytest.raises(ReleaseFileEmpty):
+        autopub.check()
 
 
-def test_fails_if_release_is_bad(tmpdir):
-    release_file = Path(tmpdir) / "RELEASE.md"
+def test_fails_if_release_is_bad(temporary_working_directory):
+    release_file = Path(temporary_working_directory) / "RELEASE.md"
     release_file.write_text("This is not a valid release note")
 
-    with tmpdir.as_cwd():
-        autopub = Autopub()
+    autopub = Autopub()
 
-        with pytest.raises(ReleaseNoteInvalid):
-            autopub.check()
+    with pytest.raises(ReleaseNoteInvalid):
+        autopub.check()
 
 
-def test_fails_if_release_file_is_missing_release_type(tmpdir):
-    release_file = Path(tmpdir) / "RELEASE.md"
+def test_fails_if_release_file_is_missing_release_type(temporary_working_directory):
+    release_file = Path(temporary_working_directory) / "RELEASE.md"
     release_file.write_text("Example \nThis is not a valid release note")
 
-    with tmpdir.as_cwd():
-        autopub = Autopub()
+    autopub = Autopub()
 
-        with pytest.raises(MissingReleaseType):
-            autopub.check()
+    with pytest.raises(MissingReleaseType):
+        autopub.check()
 
 
-def test_fails_if_release_type_is_not_valid(tmpdir):
-    release_file = Path(tmpdir) / "RELEASE.md"
+def test_fails_if_release_type_is_not_valid(temporary_working_directory):
+    release_file = Path(temporary_working_directory) / "RELEASE.md"
     release_file.write_text("Release type: butter\nThis is not a valid release note")
 
-    with tmpdir.as_cwd():
-        autopub = Autopub()
+    autopub = Autopub()
 
-        with pytest.raises(InvalidReleaseType):
-            autopub.check()
+    with pytest.raises(InvalidReleaseType):
+        autopub.check()
