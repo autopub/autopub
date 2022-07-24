14dfac63b976606699cd66893108d3d2cd8e45f0
Patrick Arminio <patrick.arminio@gmail.com>
2022-07-24 15:38:49 +0100

    Cleaner code

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,12 +1,11 @@
-import textwrap
-
 import pytest
 
+VALID_RELEASE_TEXT = """
+Release type: patch
+
+This is a new release.
+"""
 
 @pytest.fixture
 def valid_release_text()-> str:
-    return textwrap.dedent("""
-    Release type: patch
-
-    This is a new release.
-    """).strip()
+    return VALID_RELEASE_TEXT.strip()
