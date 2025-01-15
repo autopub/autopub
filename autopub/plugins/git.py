c37ee0b080e884b961308ef2a3e19bebee19a8d0
Patrick Arminio <patrick.arminio@gmail.com>
2025-01-15 11:59:20 +0000

    Commit and push

--- autopub/plugins/git.py
+++ autopub/plugins/git.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
 
+import textwrap
+
 from autopub.plugins import AutopubPlugin
 from autopub.types import ReleaseInfo
 
@@ -15,9 +17,19 @@ class GitPlugin(AutopubPlugin):
 
         self.run_command(["git", "tag", tag_name])
 
+        commit_message = textwrap.dedent(
+            f"""
+            Release {release_info.version}
+
+            {release_info.release_notes}
+
+            [skip ci]
+            """
+        )
+
         # TODO: config?
         self.run_command(["git", "rm", "RELEASE.md"])
-        self.run_command(["git", "add", "--all", "--", ":!main/.autopub"])
-        self.run_command(["git", "commit", "-m", "🤖 autopub publish"])
+        self.run_command(["git", "add", "--all", "--", ":!.autopub"])
+        self.run_command(["git", "commit", "-m", commit_message])
         self.run_command(["git", "push"])
         self.run_command(["git", "push", "origin", tag_name])
